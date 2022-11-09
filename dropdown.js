ac31f8cc859407915160b86ff97d8622bdd849c5
Caio Cesar <59320997+caiohxp@users.noreply.github.com>
2022-11-09 20:09:54 -0300

    update animation dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,6 +1,14 @@
 const menuDrop = document.querySelector('.menuDropdown');
-function toggleMenu(){
-    const menuDrop1 = document.querySelector('.menuDropdown');
-    menuDrop1.classList.toggle('active')
-}
+
 menuDrop.addEventListener('click', toggleMenu);
+function toggleMenu(){
+    const menuDrop1 = document.querySelector('#menuDropdown1');
+    const menuDrop2 = document.querySelector('#menuDropdown2');
+    const menuDrop3 = document.querySelector('#menuDropdown3');
+    menuDrop1.classList.toggle('active')
+    menuDrop2.classList.toggle('active')
+    menuDrop3.classList.toggle('active')
+    menuDrop1.classList.toggle('animationdrop1')
+    menuDrop2.classList.toggle('animationdrop1')
+    menuDrop3.classList.toggle('animationdrop3')
+}
