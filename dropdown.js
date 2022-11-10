81b17d991cde1f22c9bdf4628c6c54601a3a55fb
Caio Cesar <59320997+caiohxp@users.noreply.github.com>
2022-11-09 21:37:24 -0300

    update dropdown

--- dropdown.js
+++ dropdown.js
@@ -2,9 +2,14 @@ const menuDrop = document.querySelector('.menuDropdown');
 
 menuDrop.addEventListener('click', toggleMenu);
 function toggleMenu(){
+    const menu = document.querySelector('.menu');
+    const menuLista = document.querySelector('.menuLista');
     const menuDrop1 = document.querySelector('#menuDropdown1');
     const menuDrop2 = document.querySelector('#menuDropdown2');
     const menuDrop3 = document.querySelector('#menuDropdown3');
+    menu.classList.toggle('active')
+    menuLista.classList.toggle('active')
+    menuDrop.classList.toggle('active')
     menuDrop1.classList.toggle('active')
     menuDrop2.classList.toggle('active')
     menuDrop3.classList.toggle('active')
