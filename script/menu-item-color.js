9af7ca8c0855fb09ee5cdb63fc201e6b6189cb42
caiohxp <caiocesar.ccomp@gmail.com>
2022-11-23 23:52:09 -0300

    up style

--- script/menu-item-color.js
+++ script/menu-item-color.js
@@ -1,4 +1,4 @@
-const cor = "#14FFEC";
+const cor = "crimson";
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     const itensMenu = document.querySelectorAll('.menuItem')
@@ -7,32 +7,32 @@ window.addEventListener("scroll", (event) => {
     || document.body.clientHeight;
     if(scroll < windowHeight){
         itensMenu.forEach((i,n) => {
-            n === 0 ? i.style.color = cor : i.style.color = "white"
+            n === 0 ? i.classList.add('active') : i.classList.remove('active')
         })
     }
     else if(scroll < (windowHeight*2)){
         itensMenu.forEach((i,n) => {
-            n === 1 ? i.style.color = cor : i.style.color = "white"
+            n === 1 ? i.classList.add('active') : i.classList.remove('active')
         })
     } else if(scroll< (windowHeight*3)){
         itensMenu.forEach((i,n) => {
-            n === 2 ? i.style.color = cor : i.style.color = "white"
+            n === 2 ? i.classList.add('active') : i.classList.remove('active')
         })
     } else if(scroll < (windowHeight*4)){
         itensMenu.forEach((i,n) => {
-            n === 3 ? i.style.color = cor : i.style.color = "white"
+            n === 3 ? i.classList.add('active') : i.classList.remove('active')
         })
     } else if(scroll < (windowHeight*5)){
         itensMenu.forEach((i,n) => {
-            n === 4 ? i.style.color = cor : i.style.color = "white"
+            n === 4 ? i.classList.add('active') : i.classList.remove('active')
         })
     } else if(scroll < (windowHeight*6)){
         itensMenu.forEach((i,n) => {
-            n === 5 ? i.style.color = cor : i.style.color = "white"
+            n === 5 ? i.classList.add('active') : i.classList.remove('active')
         })
     } else{
         itensMenu.forEach((i,n) => {
-            n === 6 ? i.style.color = cor : i.style.color = "white"
+            n === 6 ? i.classList.add('active') : i.classList.remove('active')
         })
     }
 });
