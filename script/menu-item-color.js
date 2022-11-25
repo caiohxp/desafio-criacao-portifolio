ce3921d1ef65cac378b83708cd371f4162a7cb6b
caiohxp <caiocesar.ccomp@gmail.com>
2022-11-25 01:15:22 -0300

    scroolspy

--- script/menu-item-color.js
+++ script/menu-item-color.js
@@ -1,41 +1,44 @@
-const cor = "crimson";
+const itensMenu = document.querySelectorAll('.nav-item');
+const section = Array.from(document.querySelectorAll('section'));
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    const itensMenu = document.querySelectorAll('.menuItem')
-    var windowHeight = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
-    if(scroll < windowHeight){
-        itensMenu.forEach((i,n) => {
+    if (scroll < heightSections(1)) {
+        itensMenu.forEach((i, n) => {
             n === 0 ? i.classList.add('active') : i.classList.remove('active')
         })
     }
-    else if(scroll < (windowHeight*2)){
-        itensMenu.forEach((i,n) => {
+    else if (scroll < heightSections(2)) {
+        itensMenu.forEach((i, n) => {
             n === 1 ? i.classList.add('active') : i.classList.remove('active')
         })
-    } else if(scroll< (windowHeight*3)){
-        itensMenu.forEach((i,n) => {
+    } else if (scroll < heightSections(3)) {
+        itensMenu.forEach((i, n) => {
             n === 2 ? i.classList.add('active') : i.classList.remove('active')
         })
-    } else if(scroll < (windowHeight*4)){
-        itensMenu.forEach((i,n) => {
+    } else if (scroll < heightSections(4)) {
+        itensMenu.forEach((i, n) => {
             n === 3 ? i.classList.add('active') : i.classList.remove('active')
         })
-    } else if(scroll < (windowHeight*5)){
-        itensMenu.forEach((i,n) => {
+    } else if (scroll < heightSections(5)) {
+        itensMenu.forEach((i, n) => {
             n === 4 ? i.classList.add('active') : i.classList.remove('active')
         })
-    } else if(scroll < (windowHeight*6)){
-        itensMenu.forEach((i,n) => {
+    } else if (scroll < heightSections(6)) {
+        itensMenu.forEach((i, n) => {
             n === 5 ? i.classList.add('active') : i.classList.remove('active')
         })
-    } else{
-        itensMenu.forEach((i,n) => {
+    } else {
+        itensMenu.forEach((i, n) => {
             n === 6 ? i.classList.add('active') : i.classList.remove('active')
         })
     }
 });
-window.onresize = function(){
+window.onresize = function () {
     location.reload();
- };
+};
+
+function heightSections(x) {
+    return section.slice(0, x).reduce((a, b) => {
+        return a + b.clientHeight
+    }, 0) - 50
+}
