0cec7a77a9e3c8eb6fbdd5bf9fab843dbcb61374
caiohxp <caiocesar.ccomp@gmail.com>
2022-11-26 04:03:19 -0300

    add-icons final-contato up-sobre

--- script/menu-item-color.js
+++ script/menu-item-color.js
@@ -1,35 +1,36 @@
 const itensMenu = document.querySelectorAll('.nav-item');
 const section = Array.from(document.querySelectorAll('section'));
+const cor = "#14FFEC"
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     if (scroll < heightSections(1)) {
         itensMenu.forEach((i, n) => {
-            n === 0 ? i.classList.add('active') : i.classList.remove('active')
+            n === 0 ? i.style.color = cor : i.style.color = "#fff"
         })
     }
     else if (scroll < heightSections(2)) {
         itensMenu.forEach((i, n) => {
-            n === 1 ? i.classList.add('active') : i.classList.remove('active')
+            n === 1 ? i.style.color = cor : i.style.color = "#fff"
         })
     } else if (scroll < heightSections(3)) {
         itensMenu.forEach((i, n) => {
-            n === 2 ? i.classList.add('active') : i.classList.remove('active')
+            n === 2 ? i.style.color = cor : i.style.color = "#fff"
         })
     } else if (scroll < heightSections(4)) {
         itensMenu.forEach((i, n) => {
-            n === 3 ? i.classList.add('active') : i.classList.remove('active')
+            n === 3 ? i.style.color = cor : i.style.color = "#fff"
         })
     } else if (scroll < heightSections(5)) {
         itensMenu.forEach((i, n) => {
-            n === 4 ? i.classList.add('active') : i.classList.remove('active')
+            n === 4 ? i.style.color = cor : i.style.color = "#fff"
         })
     } else if (scroll < heightSections(6)) {
         itensMenu.forEach((i, n) => {
-            n === 5 ? i.classList.add('active') : i.classList.remove('active')
+            n === 5 ? i.style.color = cor : i.style.color = "#fff"
         })
     } else {
         itensMenu.forEach((i, n) => {
-            n === 6 ? i.classList.add('active') : i.classList.remove('active')
+            n === 6 ? i.style.color = cor : i.style.color = "#fff"
         })
     }
 });
