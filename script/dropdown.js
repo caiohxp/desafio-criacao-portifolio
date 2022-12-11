37ebb3c64483f744089a46e9d00ee85fa451cd8d
caiohxp <caiocesar.ccomp@gmail.com>
2022-12-11 05:07:01 -0300

    button color

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,7 +1,6 @@
+const menuDrop = document.querySelector('.menuDropdown');
+menuDrop.addEventListener('click', toggleMenu);
 function toggleMenu(){
-    const menuDrop = document.querySelector('.menuDropdown');
-    
-    menuDrop.addEventListener('click', toggleMenu);
     const menu = document.querySelector('.menu');
     menu.classList.toggle('active');   
 }
